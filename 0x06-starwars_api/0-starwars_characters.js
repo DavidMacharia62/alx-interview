928ec7d22feb195ce2613b33c1960f709943170b
DavidMacharia62 <davidmacharia1@gmail.com>
2024-01-18 02:20:04 -0800

    starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,34 @@
 #!/usr/bin/node
-/**
- * a script that prints all characters of a Star Wars movie
- * use star wars api
- */
+
+const process = require('process');
 const request = require('request');
-const film_id = process.argv[2];
-if (!film_id  || isNaN(film_id)) {
-  process.exit(1);
+const arg = process.argv[2];
+
+const printCharacters = () => {
+  request(`https://swapi-api.alx-tools.com/api/films/${arg}`, async (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+
+    const characterUrls = JSON.parse(body).characters;
+    for (const characterUrl of characterUrls) {
+      const url = await printACharacter(characterUrl);
+      console.log(url);
+    }
+  });
+};
+
+const printACharacter = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      resolve(JSON.parse(body).name);
+    });
+  });
+};
+
+if (process.argv.length > 2) {
+  printCharacters();
 }
-const url = `https://swapi-api.hbtn.io/api/films/${film_id}`;
-
-request(url, (error, res, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const characterList = [];
-
-  const json = JSON.parse(body);
-  const characters = json.characters;
-
-  characters.forEach((character) => {
-    const url = character;
-    const promise = new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        const json = JSON.parse(body);
-        resolve(json.name);
-      });
-    });
-    characterList.push(promise);
-  });
-  Promise.all(characterList).then((values) => {
-    values.forEach((value) => {
-      console.log(value);
-    });
-  }).catch((error) => {
-    console.log(error);
-  });
-});
